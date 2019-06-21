c28668480de0df1ff01046b946f532c78d1f0d61
Kevin Barabash <kevinb@khanacademy.org>
2019-06-20 21:36:30 -0400

    add tests for overloading functions

---
+++ tests/cases/compiler/strictAssignment4.ts
@@ -0,0 +1,28 @@
+// @strictAssignment: true
+
+module StrictAssignment4 {
+    class Animal {}
+    class Cat { purr() {} }
+    class Dog { bark() {} }
+    
+    const cats: Cat[] = [new Cat];
+    
+    function foo(animals: Cat[]): void
+    function foo(animals: Animal[]): void {
+    }
+    foo(cats); // okay, because there's a safe overload
+    foo([new Cat]); // okay, because there's a safe overload
+
+    function oof(animals: Animal[]): void 
+    function oof(animals: Cat[]): void {
+    }
+    // TODO: make it so this isn't an error
+    oof(cats); // okay, because there's a safe overload
+    oof([new Cat]); // okay, because there's a safe overload
+
+    function bar(animals: Dog[]): void
+    function bar(animals: Animal[]): void {
+    }
+    bar(cats); // error, there's no safe overload
+    bar([new Cat]); // error, there's no safe overload
+}
